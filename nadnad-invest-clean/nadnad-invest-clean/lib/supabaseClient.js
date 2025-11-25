ffd2d1d11cd59842b43c6a56199eb039ded22905
mr sn <sonnnn603@gmail.com>
2025-11-25 21:47:41 +0700

    Update supabaseClient.js

--- nadnad-invest-clean/nadnad-invest-clean/lib/supabaseClient.js
+++ nadnad-invest-clean/nadnad-invest-clean/lib/supabaseClient.js
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  console.warn("Supabase env vars belum di-set di Vercel.");
+  console.warn("⚠️ Supabase URL atau ANON KEY belum di-set di Vercel.");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
