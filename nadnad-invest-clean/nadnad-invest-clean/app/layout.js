d57a923a50317d6793bb36281169cc0f25b4d2ce
mr sn <sonnnn603@gmail.com>
2025-11-28 20:49:22 +0700

    Update layout.js

--- nadnad-invest-clean/nadnad-invest-clean/app/layout.js
+++ nadnad-invest-clean/nadnad-invest-clean/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata = {
   title: "Nanad Invest",
-  description: "Personal Planning & Simulation Space",
+  description: "Nanad Invest · Personal Planning & Wealth Space",
 };
 
 export default function RootLayout({ children }) {
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
 
         {/* TOMBOL PENGADUAN WHATSAPP NANAD INVEST */}
         <a
-          href="https://wa.me/6281234567890?text=Halo%20admin%20Nanad%20Invest%2C%20saya%20ingin%20mengajukan%20pengaduan%20terkait%20rekening%20tujuan%20deposit."
+          href="https://wa.me/6281234567890?text=Halo%20admin%20Nanad%20Invest%2C%20saya%20ingin%20mengkonfirmasi%20rekening%20tujuan%20atau%20pengajuan%20transaksi."
           target="_blank"
           rel="noreferrer"
           className="nanad-wa-fab"
