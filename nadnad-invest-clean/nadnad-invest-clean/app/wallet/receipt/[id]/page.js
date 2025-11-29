12187d764eaba6f556b2173063138b460b89f374
mr sn <sonnnn603@gmail.com>
2025-11-29 15:10:34 +0700

    Update page.js

--- nadnad-invest-clean/nadnad-invest-clean/app/wallet/receipt/[id]/page.js
+++ nadnad-invest-clean/nadnad-invest-clean/app/wallet/receipt/[id]/page.js
@@ -4,7 +4,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import supabase from "../../../lib/supabaseClient"; // ✅ ini yang benar
+import supabase from "../../../../lib/supabaseClient"; // ✅ ini yang benar
 
 function formatCurrency(value) {
   return new Intl.NumberFormat("id-ID", {
