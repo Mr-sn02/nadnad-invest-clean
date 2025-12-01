f4441d069ebd8ea2323573ce2688f360bbf95a30
mr sn <sonnnn603@gmail.com>
2025-12-01 16:36:24 +0700

    Update page.js

--- nadnad-invest-clean/nadnad-invest-clean/app/admin/promo/balance-boost/page.js
+++ nadnad-invest-clean/nadnad-invest-clean/app/admin/promo/balance-boost/page.js
@@ -3,7 +3,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "../../../lib/supabaseClient";
+import supabase from "../../../../lib/supabaseClient";
 
 // ADMIN EMAIL SEDERHANA
 const ADMIN_EMAILS = ["sonnnn603@gmail.com"];
