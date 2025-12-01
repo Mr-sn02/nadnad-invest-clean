03fc09b37b1abf1f2e2ba8284800b4d69c21f9a1
mr sn <sonnnn603@gmail.com>
2025-12-01 13:41:58 +0700

    Update page.js

--- nadnad-invest-clean/nadnad-invest-clean/app/promo/balance-boost/page.js
+++ nadnad-invest-clean/nadnad-invest-clean/app/promo/balance-boost/page.js
@@ -3,7 +3,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import supabase from "../../../../../lib/supabaseClient";
+import supabase from "../../../lib/supabaseClient";
 
 // GANTI DENGAN EMAIL ADMINMU
 const ADMIN_EMAILS = ["adminmu@contoh.com"];
